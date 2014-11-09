94ebbd68c74ae244488a865a6bed3bb049650c52
benweddel <benweddel@rogers.com>
2014-11-09 11:45:42 -0500

    more changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,31 @@
 var portApp = {};
 
+portApp.s1 = $('#social1');
+portApp.s2 = $('#social2');
+portApp.s3 = $('#social3');
+portApp.s4 = $('#social4');
+
+portApp.danceSpeed = 220;
+portApp.danceDelay = 150;
+
+
+
+
 portApp.init = function(){
-  
+  $('#contact').on('click', function(){
+    portApp.dance();
+  });
+};
+
+portApp.dance = function(){
+  portApp.s1.animate({'top':-25},portApp.danceSpeed);
+  portApp.s1.animate({'top':0},portApp.danceSpeed);
+  portApp.s2.delay(portApp.danceDelay).animate({'top':-25},portApp.danceSpeed);
+  portApp.s2.animate({'top':0},portApp.danceSpeed);
+  portApp.s3.delay(portApp.danceDelay*2).animate({'top':-25},portApp.danceSpeed)
+  portApp.s3.animate({'top':0},portApp.danceSpeed)
+  portApp.s4.delay(portApp.danceDelay*3).animate({'top':-25},portApp.danceSpeed)
+  portApp.s4.animate({'top':0},portApp.danceSpeed)
 };
 
 $(function(){
